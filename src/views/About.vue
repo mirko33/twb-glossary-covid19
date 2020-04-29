a334ffd14fe2a1638712648750f67e26d73d8347
Mirko Plitt <mirko@modulolanguage.com>
2020-04-29 12:58:40 +0200

    Second commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+ <v-container>
+<h3 style="color:black">TWB COVID-19 Glossary</h3>
+<p style="color:black">Webapp created from terminology curated, translated and maintained by Translators without Borders, as part of the COVID-19 response.</p>
+<p style="color:black">This is a Progressive Web App and can therefore be opened on computers as well as phones and tablets. If installed or bookmarked, it can also be used when offline.</p>
+<p style="color:black">For contributions or other inquiries, please contact <a href="mailto:info@translatorswithoutborders.org">info@translatorswithoutborders.org</a>.</p>
+ </v-container>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'publicPath': '',
+  "assetsDir": 'src/assets',
+  "pwa": {
+    "name": 'TWB COVID-19 Glossary',
+    "themeColor": "#e8991c",
+    "msTileColor": "#e8991c",
+    "appleMobileWebAppCapable": 'yes',
+    "manifestOptions": {
+      "background_color": "#e8991c"
+    }
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: /glossary.json$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'twb-glossary-covid-19'
+          }
+        }
+      ]
+    },
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
